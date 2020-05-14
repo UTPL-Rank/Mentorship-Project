0fbf17f96700e7a752b6f6f4713d10f8fe33bac1
Bruno Esparza <bruno.be81@gmail.com>
2020-05-14 16:10:08 -0500

    chore: add dev-version of the project

--- projects/webapp/src/app/core/modules/firebase.module.ts
+++ projects/webapp/src/app/core/modules/firebase.module.ts
@@ -6,21 +6,11 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
 import { AngularFirePerformanceModule } from '@angular/fire/performance';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-
-const FIREBASE_CONFIGURATION = {
-  apiKey: 'AIzaSyDSzoAXm8KmN5Z8dWCVckrygK-ESDUngag',
-  authDomain: 'sgmentores.firebaseapp.com',
-  databaseURL: 'https://sgmentores.firebaseio.com',
-  projectId: 'sgmentores',
-  storageBucket: 'sgmentores.appspot.com',
-  messagingSenderId: '395423068727',
-  appId: '1:395423068727:web:6052e03ffd5a6ffb8d85b2',
-  measurementId: 'G-FKJKMMSBLH'
-};
+import { environment } from 'projects/webapp/src/environments/environment';
 
 @NgModule({
   imports: [
-    AngularFireModule.initializeApp(FIREBASE_CONFIGURATION),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule.enablePersistence(),
     AngularFireStorageModule,
     AngularFireAuthModule,
--- projects/webapp/src/environments/environment.prod.ts
+++ projects/webapp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDSzoAXm8KmN5Z8dWCVckrygK-ESDUngag',
+    authDomain: 'sgmentores.firebaseapp.com',
+    databaseURL: 'https://sgmentores.firebaseio.com',
+    projectId: 'sgmentores',
+    storageBucket: 'sgmentores.appspot.com',
+    messagingSenderId: '395423068727',
+    appId: '1:395423068727:web:6052e03ffd5a6ffb8d85b2',
+    measurementId: 'G-FKJKMMSBLH'
+  }
 };
