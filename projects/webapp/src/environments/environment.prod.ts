0f73c9b767bd83e434c50cfeae67fd039b7222cc
Bruno Esparza <bruno.be81@gmail.com>
2020-06-08 09:03:41 -0500

    fix: pwa service

--- projects/webapp/src/environments/environment.prod.ts
+++ projects/webapp/src/environments/environment.prod.ts
@@ -9,5 +9,8 @@ export const environment = {
     messagingSenderId: '395423068727',
     appId: '1:395423068727:web:6052e03ffd5a6ffb8d85b2',
     measurementId: 'G-FKJKMMSBLH'
+  },
+  messaging: {
+    serverKey: 'AAAAXBENMjc:APA91bEUyppWzkzuDGdbx-Dq351ABRyIgajWrhMDz8qyhXNYemx6B1JUe9kY89niqgJ3B36BCs5FI1sM-e-opNGgujV7_oApW76GM7lNpDd1Y05mc5nDf0ndDidzazUBYGKgfibTnKlZ'
   }
 };
