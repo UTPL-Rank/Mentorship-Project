65e723eff19d13f83a8fc7d51acbb11814db372e
Bruno Esparza <bruno.be81@gmail.com>
2020-06-08 09:03:41 -0500

    fix: pwa service

--- frontend/projects/webapp/src/app/dashboard/pages/dashboard-home/dashboard-home.page.ts
+++ frontend/projects/webapp/src/app/dashboard/pages/dashboard-home/dashboard-home.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'firebase/app';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthenticationService } from '../../../core/services/authentication.service';
 import { TitleService } from '../../../core/services/title.service';
 
@@ -8,20 +8,16 @@ import { TitleService } from '../../../core/services/title.service';
   selector: 'sgm-dashboard-home',
   templateUrl: './dashboard-home.page.html'
 })
-export class DashboardHomePage implements OnInit, OnDestroy {
-  constructor(private auth: AuthenticationService, private title: TitleService) { }
+export class DashboardHomePage implements OnInit {
+  constructor(
+    private readonly auth: AuthenticationService,
+    private readonly title: TitleService,
+  ) { }
 
-  private authSub: Subscription;
-  user: User;
+  user: Observable<User> = this.auth.currentUser;
 
   async ngOnInit() {
     this.title.setTitle('Panel de Control');
-
-    this.authSub = this.auth.currentUser
-      .subscribe(user => this.user = user);
   }
 
-  ngOnDestroy() {
-    this.authSub.unsubscribe();
-  }
 }
