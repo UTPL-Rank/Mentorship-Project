f26ad9d68706e87a76c9dc06a800eed0370fa286
Bruno Esparza <bruno.be81@gmail.com>
2020-06-15 21:02:32 -0500

    feat(final-evaluation): add component

---
+++ projects/webapp/src/app/mentors/components/evaluation-form/evaluation-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sgm-evaluation-form',
+  templateUrl: './evaluation-form.component.html'
+})
+
+export class EvaluationFormComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
