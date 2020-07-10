70b3142c901fbad20177eaff4a288912b19f6c44
Bruno Esparza <bruno.be81@gmail.com>
2020-07-09 20:12:18 -0500

    feat(notifications): add style unread and read functionality

--- frontend/projects/webapp/src/app/dashboard/components/notifications/notifications.component.ts
+++ frontend/projects/webapp/src/app/dashboard/components/notifications/notifications.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { mergeMap } from 'rxjs/operators';
 import { AuthenticationService } from '../../../core/services/authentication.service';
+import { Notification } from '../../../models/models';
 
 @Component({
   selector: 'sgm-notifications',
@@ -10,5 +13,14 @@ export class NotificationsComponent {
 
   constructor(
     public readonly auth: AuthenticationService,
+    private readonly router: Router,
   ) { }
+
+  async handleRedirect(n: Notification) {
+    const update = this.auth.toggleNotificationRead(n.id).pipe(
+      mergeMap(async _ => await this.router.navigateByUrl(n.redirect)),
+    );
+
+    await update.toPromise();
+  }
 }
