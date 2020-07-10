1b0ae247b708e8b0f46635580de83b4caf93caad
Bruno Esparza <bruno.be81@gmail.com>
2020-07-10 16:10:52 -0500

    feat(evaluation): add stucture

--- frontend/projects/webapp/src/app/mentors/mentors.module.ts
+++ frontend/projects/webapp/src/app/mentors/mentors.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { BestMentorsComponent } from './components/best-mentors/best-mentors.component';
-import { EvaluationActivitiesComponent } from './components/evaluation-activities/evaluation-activities.component';
-import { EvaluationDependenciesComponent } from './components/evaluation-dependencies/evaluation-dependencies.component';
-import { EvaluationObservationsComponent } from './components/evaluation-observations/evaluation-observations.component';
 import { MentorHeaderComponent } from './components/mentor-header/mentor-header.component';
 import { MentorNavbarComponent } from './components/mentor-navbar/mentor-navbar.component';
 import { MentorStatsComponent } from './components/mentor-stats/mentor-stats.component';
@@ -28,9 +25,6 @@ import { MentorsRoutingModule } from './mentors-routing.module';
     MentorStatsComponent,
     MentorHeaderComponent,
     MentorNavbarComponent,
-    EvaluationActivitiesComponent,
-    EvaluationObservationsComponent,
-    EvaluationDependenciesComponent,
   ],
 })
 export class MentorsModule { }
