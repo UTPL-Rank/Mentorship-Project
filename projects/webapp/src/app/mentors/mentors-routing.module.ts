e2d979970813afcc9f9fd596807ec2c0194efe9f
Bruno Esparza <bruno.be81@gmail.com>
2020-07-10 16:10:52 -0500

    feat(evaluation): add stucture

--- projects/webapp/src/app/mentors/mentors-routing.module.ts
+++ projects/webapp/src/app/mentors/mentors-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { IsAdminGuard } from '../core/guards/is-admin.guard';
 import { IsMentorGuard } from '../core/guards/is-mentor.guard';
 import { ValidPeriodOfMentorGuard } from '../core/guards/valid-period-of-mentor.guard';
-import { FinalEvaluationComponent } from './pages/final-evaluation/final-evaluation.component';
+import { EvaluationActivitiesComponent } from './pages/final-evaluation/evaluation-activities.component';
+import { EvaluationDependenciesComponent } from './pages/final-evaluation/evaluation-dependencies.component';
+import { EvaluationObservationsComponent } from './pages/final-evaluation/evaluation-observations.component';
 import { ListAssignedStudentsComponent } from './pages/list-assigned-students/list-assigned-students.component';
 import { ListMentorsComponent } from './pages/list-mentors/list-mentors.component';
 import { ViewMentorHistoryComponent } from './pages/view-mentor-history/view-mentor-history.component';
@@ -17,7 +19,16 @@ const routes: Routes = [
   { path: ':mentorId', component: ViewMentorComponent, canActivate: [IsMentorGuard, ValidPeriodOfMentorGuard], },
   { path: ':mentorId/estudiantes', component: ListAssignedStudentsComponent, canActivate: [IsMentorGuard, ValidPeriodOfMentorGuard], },
   { path: ':mentorId/historial', component: ViewMentorHistoryComponent, canActivate: [IsMentorGuard, ValidPeriodOfMentorGuard], },
-  { path: ':mentorId/evaluacion-final', component: FinalEvaluationComponent, canActivate: [IsMentorGuard, ValidPeriodOfMentorGuard], },
+  {
+    path: ':mentorId/evaluacion-final',
+    canActivate: [IsMentorGuard, ValidPeriodOfMentorGuard],
+    children: [
+      { path: 'actividades', component: EvaluationActivitiesComponent },
+      { path: 'dependencias', component: EvaluationDependenciesComponent },
+      { path: 'observaciones', component: EvaluationObservationsComponent },
+      { path: '**', redirectTo: 'actividades' },
+    ]
+  },
 ];
 
 
@@ -31,6 +42,8 @@ export class MentorsRoutingModule {
     ViewMentorComponent,
     ViewMentorHistoryComponent,
     ListAssignedStudentsComponent,
-    FinalEvaluationComponent,
+    EvaluationActivitiesComponent,
+    EvaluationDependenciesComponent,
+    EvaluationObservationsComponent,
   ];
 }
