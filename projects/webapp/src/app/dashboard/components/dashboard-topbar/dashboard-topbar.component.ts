e8313a5a3dd40c31ed30763a28491da386d6e47d
Bruno Esparza <bruno.be81@gmail.com>
2020-07-10 11:26:15 -0500

    fix: remove topbar unused services

--- projects/webapp/src/app/dashboard/components/dashboard-topbar/dashboard-topbar.component.ts
+++ projects/webapp/src/app/dashboard/components/dashboard-topbar/dashboard-topbar.component.ts
@@ -1,49 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { User } from 'firebase/app';
-import { Subscription } from 'rxjs';
-import { AcademicPeriodsService } from '../../../core/services/academic-periods.service';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../core/services/authentication.service';
-import { AcademicPeriod, AcademicPeriods, UserClaims } from '../../../models/models';
 
 @Component({
   selector: 'sgm-dashboard-topbar',
   templateUrl: './dashboard-topbar.component.html',
   styleUrls: ['./dashboard-topbar.component.scss']
 })
-export class DashboardTopbarComponent implements OnInit, OnDestroy {
+export class DashboardTopbarComponent implements OnInit {
   constructor(
     private readonly auth: AuthenticationService,
-    private readonly periodService: AcademicPeriodsService,
-    private readonly route: ActivatedRoute
   ) { }
 
-  private userSub: Subscription;
-  private claimsSub: Subscription;
-  private dataSub: Subscription;
-
-  public activePeriod: AcademicPeriod;
-  public periods: AcademicPeriods;
-  public user: User;
-  public claims: UserClaims;
-  ngOnInit(): void {
-    this.periods = this.periodService.loadedPeriods;
-
-    this.userSub = this.auth.currentUser
-      .subscribe(user => this.user = user);
-
-    this.claimsSub = this.auth.claims
-      .subscribe(claims => this.claims = claims);
-
-    this.dataSub = this.route.data
-      .subscribe(data => this.activePeriod = data.activePeriod);
-  }
-
-  ngOnDestroy(): void {
-    this.userSub.unsubscribe();
-    this.dataSub.unsubscribe();
-    this.claimsSub.unsubscribe();
-  }
+  ngOnInit() { }
 
   async logout() {
     await this.auth.signOut(['/']);
