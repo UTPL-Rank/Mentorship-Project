76b8fea03c032250212417fa18bca33b85dab55e
Bruno Esparza <bruno.be81@gmail.com>
2020-08-19 18:10:09 -0500

    feat(messaging tokens): add methods to save or remove a messaging token of users

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './auth/on-create';
 export * from './callable/csv-mentors';
 export * from './callable/csv-student';
 export * from './callable/export-accompaniment';
+export * from './callable/messaging-token';
 export * from './cron/backup-firestore';
 export * from './cron/calculate-home-statistics';
 export * from './cron/notify-mentors-accompaniments';
---
+++ src/utils/users-utils.ts
@@ -0,0 +1,83 @@
+import { firestore } from "firebase-admin";
+import { dbFirestore } from "./utils";
+
+type User = any;
+type UserProtectedDocument = any;
+
+/**
+ * Users Collection
+ * =============================================================
+ * 
+ * @author Bruno Esparza
+ * 
+ * Get the firestore collection of users
+ */
+function UsersCollection(): firestore.CollectionReference<User> {
+    return dbFirestore.collection('users');
+}
+
+/**
+ * User Document
+ * ==============================================================
+ * 
+ * @author Bruno Esparza
+ * 
+ * Get the firestore document of a user
+ * 
+ * @param username identifier of the user
+ */
+function UserDocument(username: string): firestore.DocumentReference<User> {
+    return UsersCollection().doc(username);
+}
+
+/**
+ * User Protected Information Document
+ * ==============================================================
+ * 
+ * @author Bruno Esparza
+ * 
+ * Get the firestore document of the protected information of the user
+ * 
+ * @param username identifier of the user
+ */
+function UserProtectedDocument(username: string): firestore.DocumentReference<UserProtectedDocument> {
+    return UserDocument(username).collection('account-configuration').doc('protected-information');
+}
+
+/**
+ * Save Messaging Token
+ * ==================================================================
+ * 
+ * @author Bruno Esparza
+ * 
+ * Save a token in the user protected information
+ * 
+ * @param username identifier of the user
+ * @param token messaging token to be saved
+ */
+export async function AddUserMessagingToken(username: string, token: string): Promise<void> {
+    const protectedDoc = UserProtectedDocument(username);
+    const data = {
+        tokens: firestore.FieldValue.arrayUnion(token),
+    };
+    await protectedDoc.update(data);
+}
+
+/**
+ * Remove Messaging Token
+ * ==================================================================
+ * 
+ * @author Bruno Esparza
+ * 
+ * Remove a messaging token from the user protected information
+ * 
+ * @param username identifier of the user
+ * @param token messaging token to be saved
+ */
+export async function RemoveUserMessagingToken(username: string, token: string): Promise<void> {
+    const protectedDoc = UserProtectedDocument(username);
+    const data = {
+        tokens: firestore.FieldValue.arrayRemove(token),
+    };
+    await protectedDoc.update(data);
+}
