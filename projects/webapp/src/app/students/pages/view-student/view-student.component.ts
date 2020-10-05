555d7f9d3c556eba38cbcf353cdbdaf069c37637
Bruno Esparza <bruno.be81@gmail.com>
2020-10-05 11:20:46 -0500

    feat(*): unify student history and information

--- projects/webapp/src/app/students/pages/view-student/view-student.component.ts
+++ projects/webapp/src/app/students/pages/view-student/view-student.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { AccompanimentsService } from '../../../core/services/accompaniments.service';
 import { StudentsService } from '../../../core/services/students.service';
 import { TitleService } from '../../../core/services/title.service';
-import { AcademicPeriod, Student } from '../../../models/models';
+import { AcademicPeriod, FirestoreAccompaniments, Student } from '../../../models/models';
 
 @Component({
   selector: 'sgm-view-student',
@@ -16,15 +17,33 @@ export class ViewStudentComponent {
     private readonly title: TitleService,
     private readonly route: ActivatedRoute,
     private readonly studentsService: StudentsService,
+    private readonly accompanimentsService: AccompanimentsService,
   ) { }
-
-  public readonly studentObs: Observable<Student | undefined> = this.route.params
-    .pipe(
-      switchMap(params => this.studentsService.studentStream(params.studentId)),
-      tap(student => this.title.setTitle(student.displayName.toUpperCase())),
-    );
 
   public readonly period: Observable<AcademicPeriod> = this.route.data
     .pipe(map(d => d.activePeriod));
+
+  public readonly studentObs: Observable<Student> = this.route.params
+    .pipe(
+      switchMap(params => this.studentsService.studentStream(params.studentId)),
+      tap(student => this.title.setTitle(`Estudiante ${student.displayName.toUpperCase()}`)),
+    );
+
+  public readonly periodObs: Observable<AcademicPeriod> = this.route.data
+    .pipe(map(d => d.activePeriod));
+
+
+  public readonly accompanimentsObs: Observable<FirestoreAccompaniments> = this.route.params
+    .pipe(
+      switchMap(({ periodId, studentId }) => this.accompanimentsService.accompanimentsStream({ where: { periodId, studentId } })),
+      shareReplay(1),
+      map(a => [...a])
+    );
+
+  public readonly validatedObs: Observable<FirestoreAccompaniments> = this.accompanimentsObs
+    .pipe(map(accompaniments => accompaniments.filter(a => !a.reviewKey)));
+
+  public readonly unValidatedObs: Observable<FirestoreAccompaniments> = this.accompanimentsObs
+    .pipe(map(accompaniments => accompaniments.filter(a => a.reviewKey)));
 
 }
--- projects/webapp/src/app/students/students-routing.module.ts
+++ projects/webapp/src/app/students/students-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentOfMentorGuard } from './guards/student-of-mentor.guard';
 import { GenerateStudentReportComponent } from './pages/generate-student-report/generate-student-report.component';
-import { ViewStudentHistoryComponent } from './pages/view-student-history/view-student-history.component';
 import { ViewStudentComponent } from './pages/view-student/view-student.component';
 
 const ROUTES: Routes = [
@@ -10,7 +9,6 @@ const ROUTES: Routes = [
     path: ':studentId', canActivate: [StudentOfMentorGuard],
     children: [
       { path: 'informacion', component: ViewStudentComponent, },
-      { path: 'historial', component: ViewStudentHistoryComponent, },
       { path: 'ficha-estudiante', component: GenerateStudentReportComponent, },
       { path: '', redirectTo: 'informacion' },
     ]
@@ -25,7 +23,6 @@ export class StudentsRoutingModule {
 
   static pages = [
     ViewStudentComponent,
-    ViewStudentHistoryComponent,
     GenerateStudentReportComponent,
   ];
 
