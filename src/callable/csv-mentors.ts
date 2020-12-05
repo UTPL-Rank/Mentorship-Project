aa6a919bef0331d75f3ad36f282e02d23f4708ff
Bruno Esparza <bruno.be81@gmail.com>
2020-12-05 17:03:53 -0500

    feat(mentos): add export

--- src/callable/csv-mentors.ts
+++ src/callable/csv-mentors.ts
@@ -16,31 +16,24 @@ import * as functions from 'firebase-functions';
 import { CSVFormat } from '../shared/export/csv-format';
 import { IExportCallback } from '../shared/export/i-export-callback';
 import { ListMentorsCurrentPeriod, Mentor } from '../utils/mentors-utils';
-import { GenerateCSVFromObjects } from '../utils/utils';
 
-export const CSVMentors2 = functions.https.onCall(async (data, context) => {
+export const CSVMentors = functions.https.onCall(async (data, context) => {
     const mentors = await ListMentorsCurrentPeriod();
-    const headers = ['Index', 'Nombre del Mentor'];
+    const headers = ['Index', 'Nombre del Mentor', 'Correo Electrónico', 'Área Académica', 'Titulación', 'Acompañamientos Realizados', 'Estudiantes Asignados'];
 
     const callback: IExportCallback<Mentor> = (mentor, i, arr) => {
         return [
             `Nro. ${i + 1} de ${arr.length}`,
             mentor.displayName,
+            mentor.email,
+            mentor.area.name,
+            mentor.degree.name,
+            mentor.stats.accompanimentsCount,
+            mentor.stats.assignedStudentCount,
         ];
     }
 
     const exporter = new CSVFormat<Mentor>(headers, mentors);
 
     return exporter.export(callback);
-});
-
-export const CSVMentors = functions.https.onCall(async (data, context) => {
-    const mentors = await ListMentorsCurrentPeriod();
-
-    const columnsNames = ['Nombre Mentor', 'Cedula de Identidad', 'Correo Electrónico', 'Área Académica', 'Titulación', 'Acompañamientos Realizados', 'Estudiantes Asignados'];
-    const columnKeys = ['displayName', 'ci', 'email', 'area.name', 'degree.name', 'stats.accompanimentsCount', 'stats.assignedStudentCount'];
-
-    const csv = GenerateCSVFromObjects(mentors, columnKeys, columnsNames, ',');
-
-    return csv;
 });
