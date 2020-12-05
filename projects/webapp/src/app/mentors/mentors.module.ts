3742894ddf987ba8c8613c818b0a6f981811ce2f
Bruno Esparza <bruno.be81@gmail.com>
2020-12-05 14:28:10 -0500

    feat8mentors): remove unused component

--- projects/webapp/src/app/mentors/mentors.module.ts
+++ projects/webapp/src/app/mentors/mentors.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { BestMentorsComponent } from './components/best-mentors/best-mentors.component';
 import { MentorsTableComponent } from './components/mentors-table/mentors-table.component';
 import { MentorsRoutingModule } from './mentors-routing.module';
 
@@ -16,7 +15,6 @@ import { MentorsRoutingModule } from './mentors-routing.module';
   declarations: [
     MentorsRoutingModule.pages,
     MentorsTableComponent,
-    BestMentorsComponent,
   ],
 })
 export class MentorsModule { }
