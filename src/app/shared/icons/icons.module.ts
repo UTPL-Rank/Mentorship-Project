329c605ae2cb4b27c8f35d793e35d01530c0234d
Bruno Esparza <bruno.be81@gmail.com>
2021-03-12 12:53:34 -0500

    feat(*): changed some icons

--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { FeatherModule } from 'angular-feather';
-import { ChevronLeft } from 'angular-feather/icons';
+import {
+  Activity,
+  AlertCircle,
+  Archive,
+  ChevronLeft,
+  FilePlus,
+  FileText,
+  Home,
+  List,
+  MessageCircle,
+  PieChart,
+  Upload,
+  Users
+} from 'angular-feather/icons';
 
 // Check all icons available: https://github.com/michaelbazos/angular-feather
 
 // Select some icons (use an object, not an array)
 const icons = {
   ChevronLeft,
+  Home,
+  PieChart,
+  Activity,
+  List,
+  MessageCircle,
+  AlertCircle,
+  Upload,
+  FilePlus,
+  FileText,
+  Users,
+  Archive,
 };
 
 @NgModule({
