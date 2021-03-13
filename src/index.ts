a199b5bf2c3041c829e5fcdf2d86f77c48398d66
Bruno Esparza <bruno.be81@gmail.com>
2021-03-13 12:06:51 -0500

    feat(chat); add update last chat

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export * from './cron/backup-firestore';
 export * from './cron/notify-mentors-accompaniments';
 // firestore
 export * from './firestore/accompaniments/new-accompaniment';
+export * from './firestore/chats';
 export * from './firestore/users/notify-user';
 export * from './firestore/users/send-user-emails';
 export * from './firestore/users/update-custom-claims';
