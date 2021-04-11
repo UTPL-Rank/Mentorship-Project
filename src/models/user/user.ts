918faee7075f9f54351f6b96fa75f021ef4f911b
Bruno Esparza <bruno.be81@gmail.coom>
2021-04-11 13:06:54 -0500

    add topic mentor

--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,3 +1,5 @@
+import * as firebaseAdmin from "firebase-admin";
+
 export namespace SGMUser {
     interface _Base {
         username?: string;
@@ -6,17 +8,27 @@ export namespace SGMUser {
         email: string;
         photoURL: string | null;
         uid: string;
+        notificationTopics?: Array<string> | firebaseAdmin.firestore.FieldValue;
     }
 
     /**
      * Get the information of any user in the platform
      */
-    export interface readDto extends _Base { }
+    export interface readDto extends _Base {
+        notificationTopics?: Array<string>;
+    }
 
     export module functions {
         export interface createDto extends _Base {
+            notificationTopics: [];
             username: string;
             disabled: false;
         }
+
+        export interface addTopicDto extends Pick<_Base, 'notificationTopics'> {
+            notificationTopics: firebaseAdmin.firestore.FieldValue;
+        }
+
+        export interface removeTopicDto extends addTopicDto { }
     }
 }
