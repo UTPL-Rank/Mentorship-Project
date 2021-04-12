670807a3b14bbf0f6e2c12c44c4f2eafb525b361
Bruno Esparza <bruno.be81@gmail.com>
2021-04-11 22:31:27 -0500

    change to single batch

--- src/app/upload/pages/upload-students/upload-students.service.ts
+++ src/app/upload/pages/upload-students/upload-students.service.ts
@@ -9,44 +9,32 @@ export class UploadStudentsService extends IBaseUploadDataService<SGMStudent.cre
 
   protected async uploadBatch(data: SGMStudent.createDTO[]): Promise<void> {
 
-    const chunks = data.reduce((acc, curr, idx) => {
-      const currentChunk = idx % 10;
-      if (!!acc[currentChunk])
-        acc[currentChunk].push(curr);
-      else
-        acc[currentChunk] = [curr];
-      return acc;
-    }, [[]] as Array<Array<SGMStudent.readDTO>>);
+    const batch = this.db.firestore.batch();
 
-    const saveTasks = chunks.map(async chunk => {
-      const batch = this.db.firestore.batch();
-      chunk.forEach(student => {
+    data.forEach(student => {
 
-        const username = student.email.split('@')[0];
-        const studentRef = this.db.collection('students').doc(student.id).ref;
-        const mentorRef = student.mentor.reference;
-        const claimsRef = this.usersService.claimsDocument(username).ref;
+      const username = student.email.split('@')[0];
+      const studentRef = this.db.collection('students').doc(student.id).ref;
+      const mentorRef = student.mentor.reference;
+      const claimsRef = this.usersService.claimsDocument(username).ref;
 
-        // data to be sabed
-        const claims: StudentClaims = { isStudent: true, mentorId: mentorRef.id, studentId: studentRef.id };
+      // data to be sabed
+      const claims: StudentClaims = { isStudent: true, mentorId: mentorRef.id, studentId: studentRef.id };
 
-        // transactions
-        batch.set(studentRef, student, { merge: true });
-        batch.set(claimsRef, claims, { merge: true });
+      // transactions
+      batch.set(studentRef, student, { merge: true });
+      batch.set(claimsRef, claims, { merge: true });
 
-        batch.update(mentorRef, 'stats.assignedStudentCount', firestore.FieldValue.increment(1));
-        batch.update(mentorRef, 'students.withAccompaniments', []);
-        batch.update(mentorRef, 'students.withoutAccompaniments', firestore.FieldValue.arrayUnion(student.displayName));
-        batch.update(mentorRef, 'students.cycles', firestore.FieldValue.arrayUnion(student.cycle));
-        batch.update(mentorRef, 'students.degrees', firestore.FieldValue.arrayUnion(student.degree.name));
+      batch.update(mentorRef, 'stats.assignedStudentCount', firestore.FieldValue.increment(1));
+      batch.update(mentorRef, 'students.withAccompaniments', []);
+      batch.update(mentorRef, 'students.withoutAccompaniments', firestore.FieldValue.arrayUnion(student.displayName));
+      batch.update(mentorRef, 'students.cycles', firestore.FieldValue.arrayUnion(student.cycle));
+      batch.update(mentorRef, 'students.degrees', firestore.FieldValue.arrayUnion(student.degree.name));
 
-      });
-
-      // batch writes
-      await batch.commit();
     });
 
-    await Promise.all(saveTasks);
+    // batch writes
+    await batch.commit();
   }
 
 }
