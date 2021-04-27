831dded47209096c15687344aab58d7523751175
Bruno Esparza <bruno.be81@gmail.com>
2021-04-26 20:22:20 -0500

    fix

--- src/app/dashboard/guards/valid-period.guard.ts
+++ src/app/dashboard/guards/valid-period.guard.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { UserService } from 'src/app/core/services/user.service';
 import { AcademicPeriodsService } from '../../core/services/academic-periods.service';
 
 /**
@@ -7,22 +9,37 @@ import { AcademicPeriodsService } from '../../core/services/academic-periods.ser
  */
 @Injectable({ providedIn: 'root' })
 export class ValidPeriodGuard implements CanActivate {
-  constructor(private period: AcademicPeriodsService, private router: Router) { }
+  constructor(
+    private period: AcademicPeriodsService,
+    private router: Router,
+    private user: UserService,
+  ) { }
 
   async canActivate({ params }: ActivatedRouteSnapshot) {
+    const periodId = params.periodId;
+
     // check if period have loaded
     if (!this.period.hasLoaded)
       await this.period.loadAcademicPeriods();
 
     // get if periodId is valid
-    const valid = this.period.loadedPeriods
+    let valid = this.period.loadedPeriods
       .map(p => p.id) // get only id's
-      .includes(params.periodId); // search for a coincide
+      .includes(periodId); // search for a coincide
 
-    if (!valid)
-      // navigate to panel, to be redirected to a valid id
-      return this.router.createUrlTree(['/panel-control']);
+    // validate access to older periods
+    const claims = await this.user.claims.pipe(take(1)).toPromise();
 
-    return true;
+    if (claims && !claims.isAdmin) {
+      // validate the period id current period
+      const coincidence = this.period.loadedPeriods.find(period => period.id === periodId);
+      valid = !!coincidence?.current;
+    }
+
+    if (valid)
+      return true;
+
+    // navigate to panel, to be redirected to a valid id
+    return this.router.createUrlTree(['/panel-control']);
   }
 }
