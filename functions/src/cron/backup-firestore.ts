b42c4c930dbaee32a1014b281f67bae1d50124c0
Danilo Mendoza <odmendoza@utpl.edu.ec>
2021-05-20 17:23:25 -0500

    Bug repaired: scheduledFirestoreExport does not works. Now, works :ok_hand:

--- functions/src/cron/backup-firestore.ts
+++ functions/src/cron/backup-firestore.ts
@@ -1,47 +1,32 @@
-import {firestore} from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import * as firestore from '@google-cloud/firestore';
 
-const client = firestore.v1.FirestoreAdminClient;
-
+const client = new firestore.v1.FirestoreAdminClient();
 
 const bucket = 'gs://sgmentores-backups';
 
-export const scheduledFirestoreExport =
-    functions
-        .pubsub
-        .schedule('30 2 * * 7')
-        .onRun(async _ => {
-            // This code does not works
-            // const projectId: string | null = process.env.GCP_PROJECT ?? process.env.GCLOUD_PROJECT ?? null;
-            // This code works
-            const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+exports.scheduledFirestoreExport = functions.pubsub
+    .schedule('30 2 * * 7')
+    .onRun((context: any) => {
 
-            if (!projectId)
-                throw new Error('Property project id not found');
+        const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT || 'sgmentores';
+        const databaseName =
+            client.databasePath(projectId, '(default)');
 
-
-            const databaseName =
-                client
-                    .databasePath(projectId, '(default)');
-
-            try {
-                const responses = await client.exportDocuments({
-                    name: databaseName,
-                    outputUriPrefix: bucket,
-                    // Leave collectionIds empty to export all collections
-                    // or set to a list of collection IDs to export,
-                    // collectionIds: ['users', 'posts']
-                    collectionIds: []
-                })
-
+        return client.exportDocuments({
+            name: databaseName,
+            outputUriPrefix: bucket,
+            // Leave collectionIds empty to export all collections
+            // or set to a list of collection IDs to export,
+            // collectionIds: ['users', 'posts']
+            collectionIds: []
+        })
+            .then((responses: any) => {
                 const response = responses[0];
                 console.log(`Operation Name: ${response['name']}`);
-
-                return responses;
-            } catch (err) {
+            })
+            .catch((err: any) => {
                 console.error(err);
                 throw new Error('Export operation failed');
-            }
-            ;
-
-        });
+            });
+    });
