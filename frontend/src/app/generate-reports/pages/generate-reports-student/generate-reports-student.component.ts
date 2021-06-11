c829bfa0082b6c0bb9797713f0ebcad9d63f436f
pfpicon1999 <pablopicon6@gmail.com>
2021-06-11 17:13:49 -0500

    generate-reports-mentor and student

--- frontend/src/app/generate-reports/pages/generate-reports-student/generate-reports-student.component.ts
+++ frontend/src/app/generate-reports/pages/generate-reports-student/generate-reports-student.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentsService } from '../../../core/services/students.service';
+import { ActivatedRoute } from '@angular/router';
+import { SGMAcademicPeriod, SGMMentor, SGMStudent } from '@utpl-rank/sgm-helpers';
+import { combineLatest, Observable, of, Subscription } from 'rxjs';
+import { map, shareReplay, switchMap, take } from 'rxjs/operators';
+import { MentorsService } from '../../../core/services/mentors.service';
+import { UserService } from '../../../core/services/user.service';
+
+export interface ListStudentsQuery {
+  mentorId?: string;
+  page?: number;
+  limit?: number;
+}
 
 @Component({
   selector: 'sgm-generate-reports-student',
@@ -8,7 +21,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateReportsStudentComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private readonly route: ActivatedRoute,
+    private readonly mentorsService: MentorsService,
+    private readonly studentsService: StudentsService,
+    public readonly auth: UserService,
+    ) { 
+
+  }
+  private params: Observable<{ periodId: string }> = this.route.params as any;
+
+  private query: Observable<ListStudentsQuery> = this.route.queryParams;
+
+  public readonly mentor$: Observable<SGMMentor.readDTO | null> = this.query.pipe(
+    switchMap(({ mentorId }) => !!mentorId ? this.mentorsService.mentorStream(mentorId) : of(null)),
+    shareReplay(1),
+  );
+
+  public isPeriodActiveObs = this.route.data.pipe(
+    map(d => (d.activePeriod as SGMAcademicPeriod.readDTO).current)
+  );
+
+  public readonly students$: Observable<Array<SGMStudent.readDTO>> = combineLatest([this.query, this.params]).pipe(
+    switchMap(([query, params]) => this.studentsService.list$({ periodId: params.periodId, mentorId: query.mentorId, limit: 50 })),
+    shareReplay(1),
+  );
+
+  public readonly showMentorName$: Observable<boolean> = this.mentor$.pipe(
+    map(m => !m)
+  );
 
   ngOnInit(): void {
   }
