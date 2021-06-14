f024e829de5d36dfebd506b211d219143f05e473
dani117m <marcelodaniel-117@hotmail.com>
2021-06-14 12:50:40 -0500

    cambios en el apartade servicios

--- frontend/src/app/generate-reports/services/area.service.ts
+++ frontend/src/app/generate-reports/services/area.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SGMAcademicPeriod, SGMMentor, SGMStudent } from '@utpl-rank/sgm-helpers';
+import { map, shareReplay, switchMap, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AreaService {
+ 
+  constructor(
+    private readonly route: ActivatedRoute
+  ) { }
 
-  constructor() { }
+  
+  public area_data = this.route.data.pipe(
+    map(d => ( d.areaId ,d.activePeriod as SGMAcademicPeriod.readDTO).current)
+  );
+
 }
+
