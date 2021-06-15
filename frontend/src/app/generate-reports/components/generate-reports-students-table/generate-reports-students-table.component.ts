a40917ef74e0c1a74354ad7e27a2f87a16dd9ff0
Danilo Mendoza <odmendoza@utpl.edu.ec>
2021-06-15 17:48:37 -0500

    Student report ready, TODO -> apply structure and data

--- frontend/src/app/generate-reports/components/generate-reports-students-table/generate-reports-students-table.component.ts
+++ frontend/src/app/generate-reports/components/generate-reports-students-table/generate-reports-students-table.component.ts
@@ -1,5 +1,12 @@
 import {Component, Input} from '@angular/core';
-import { SGMStudent } from '@utpl-rank/sgm-helpers';
+import { SGMAccompaniment, SGMMentor, SGMStudent } from '@utpl-rank/sgm-helpers';
+
+import { ScriptService } from '../../services/script.service';
+import { StudentReport } from '../../../shared/reports/student/student-report';
+import { MentorsService } from '../../../core/services/mentors.service';
+
+declare let pdfMake: any;
+declare let htmlToPdfmake: any;
 
 @Component({
   selector: 'sgm-generate-reports-students-table',
@@ -8,6 +15,20 @@ import { SGMStudent } from '@utpl-rank/sgm-helpers';
   ]
 })
 export class GenerateReportsStudentsTableComponent {
+
+  constructor(
+    private scriptService: ScriptService,
+    private mentorsService: MentorsService
+  ) {
+    this.scriptService.load('pdfMake', 'vfsFonts', 'htmlToPdfmake');
+  }
+
+  public accompaniments!: SGMAccompaniment.readDTO[] | null;
+  public student!: SGMStudent.readDTO | null;
+  public mentor!: SGMMentor.readDTO | null;
+  public semesterKind!: SGMAccompaniment.SemesterType | null;
+
+  public signature: string | undefined | null;
 
   public students!: Array<SGMStudent.readDTO>;
   public showMentorName = false;
@@ -20,6 +41,28 @@ export class GenerateReportsStudentsTableComponent {
   @Input('showMentorName')
   public set _showMentorName(show: boolean) {
     this.showMentorName = show;
+  }
+
+  generatePDF(periodId: string, mentorId: string, studentId: string): void {
+
+    this.accompaniments = null;
+    this.mentorsService.mentorStream(mentorId).subscribe(value => this.mentor = value);
+    this.student = null;
+    this.signature = null;
+    this.semesterKind = null;
+
+    // get the file data and sent to report
+
+    const html = new StudentReport({
+      accompaniments: this.accompaniments,
+      student: this.student,
+      mentor: this.mentor,
+      semesterKind: this.semesterKind,
+      signature: this.signature}).html();
+
+    const htmlFromPdfMake = htmlToPdfmake(html);
+    const documentDefinition = { content: htmlFromPdfMake };
+    pdfMake.createPdf(documentDefinition).open();
   }
 
 }
